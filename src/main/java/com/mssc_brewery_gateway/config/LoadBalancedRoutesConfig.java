6e0d89fd0597a99b935b805a1d77a2380419a7e5
Santiago Lordi <santilordi569@gmail.com>
2026-02-16 17:52:44 -0300

    Configure Gateway to use Eureka

--- src/main/java/com/mssc_brewery_gateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/com/mssc_brewery_gateway/config/LoadBalancedRoutesConfig.java
@@ -1,28 +1,27 @@
 package com.mssc_brewery_gateway.config;
 
-
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("!local-discovery")
+@Profile("local-discovery")
 @Configuration
-public class LocalHostRouteConfig {
+public class LoadBalancedRoutesConfig {
 
     @Bean
-    public RouteLocator localHostRouter(RouteLocatorBuilder builder ) {
+    public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder ) {
         return builder.routes()
                 .route(r -> r.path("/api/v1/beer/*", "/api/v1/beerUpc/*")
-                        .uri("http://localhost:8080")
+                        .uri("lb://beer-service")
                         .id("beer-service"))
                 //Route to Beer Order Service
                 .route(r -> r.path("/api/v1/customers/**", "/api/v1/orders/**")
-                        .uri("http://localhost:8081")
+                        .uri("lb://order-service")
                         .id("order-service"))
                 .route(r -> r.path("/api/v1/beer/*/inventory")
-                        .uri("http://localhost:8082")
+                        .uri("lb://inventory-service")
                         .id("inventory-service"))
                 .build();
     }
